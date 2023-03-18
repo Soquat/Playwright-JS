9aa92a4ccd8de28e98997075ee19f0a698841e3e
Michael, Soquat <michael.soquat@questax.com>
2023-03-18 08:51:18 +0100

    basic methods with examples

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,11 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+
+  // timout for one test!
   timeout: 30 * 1000,
+
+  // timeout for all assertions!
   expect: {
 
     timeout: 5000
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -4,9 +4,36 @@ const { test, expect } = require('@playwright/test');
 test('Browser Content Playwright test', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("https://amazon.com");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const title = await page.title();
+    const cardTitles = await page.locator(".card-body a");
     console.log(title);
+    //selectors
+    const username = page.locator('#username');
+    const pw = page.locator("[type='password']");
+    const signIn = page.locator("#signInBtn");
+    await username.type("Michael");
+    await pw.type("test");
+    await signIn.click();
+    const errorMessage = await page.locator("[style*='block']").textContent();
+    console.log(errorMessage);
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+
+    await username.fill("");
+    await username.fill("rahulshettyacademy");
+    await pw.fill("");
+    await pw.fill("learning");
+    await Promise.all(
+        [
+            page.waitForLoadState(),
+            signIn.click()
+        ]
+    );
+
+    //console.log(await cardTitles.nth(0).textContent());
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+
 })
 
 test('Page Playwright test', async ({ page }) => {

5f76b75933231cdf25de3ef3cb4c51056784e07f
Michael, Soquat <michael.soquat@questax.com>
2023-03-20 14:29:49 +0100

    mocking requests and responses

--- tests/NetworkTest2.spec.js
+++ tests/NetworkTest2.spec.js
@@ -3,32 +3,39 @@ const { APIUtils } = require("./utils/APIUtils")
 const orderPayload = {
     orders: [{ country: "India", productOrderedId: "6262e95ae26b7e1a10e89bf0" }]
 }
-const loginPayload = { userEmail: "anshika@gmail.com", userPassword: "Iamking@000" };
+const loginPayload = { userEmail: "rahulshetty@gmail.com", userPassword: "Iamking@00" };
+const fakePayLoadOrders = { data: [], message: "No Orders" }
 let response;
 
 test.beforeAll(async () => {
     const apiContext = await request.newContext();
     const apiUtils = new APIUtils(apiContext, loginPayload);
     response = await apiUtils.createOrder(orderPayload);
-    console.log("response")
-
-
+    console.log("response");
 });
 
 
-test('Login test', async ({ page }) => {
+test.only('Login test', async ({ page }) => {
     page.addInitScript(value => {
         window.localStorage.setItem("token", value);
     }, response.token);
 
     await page.goto("https://rahulshettyacademy.com/client");
+
+
     await page.locator("button[routerlink*='myorders']").click();
-    await page.locator("tbody").waitFor();
-    const rows = await page.locator("tbody tr");
 
-    for (let i = 0; i < await rows.count(); i++) {
 
-    }
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=64184458568c3e9fb13763fa", async (route) => {
+        await route.continue({
+            url: "https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=64185b19568c3e9fb13782be"
+
+
+        });
+    })
+
+
+    await page.locator("button:has-text('View')").first().click();
     await page.pause();
 
 });

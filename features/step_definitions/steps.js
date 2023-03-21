9aeb81d509b5e9a33bd212a5a16a7d58c8737977
Michael, Soquat <michael.soquat@questax.com>
2023-03-21 11:34:45 +0100

    runned all tests with correcting code with world constructor syntax

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,30 +1,28 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 const { POManager } = require('../../pageObjects/POManager');
-const { test, expect, playwright } = require('@playwright/test');
+const { expect } = require('@playwright/test');
+const playwright = require('@playwright/test');
 
-Given('a login to Ecommerce app with {username} and {password}', async function (username, password) {
+Given('a login to Ecommerce app with {string} and {string}', { timeout: 100 * 1000 }, async function (username, password) {
     const browser = await playwright.chromium.launch();
     const context = await browser.newContext();
-    const page = await context.newPage();
-    const poManager = new POManager(page);
-    const loginPage = poManager.getLoginPage();
-    const products = page.locator(".card-body");
+    this.page = await context.newPage();
+    this.poManager = new POManager(this.page);
+    const loginPage = this.poManager.getLoginPage();
+    const products = this.page.locator(".card-body");
     await loginPage.goTo();
-    await loginPage.validLogin(data.userName, data.password);
-    return 'pending';
+    await loginPage.validLogin(username, password);
 });
 
-When('Add {string} to cart', async function (string) {
-    const dashBoardPage = poManager.getDashboarPage();
-    await dashBoardPage.searchProductAddCart(data.productName);
+When('Add {string} to cart', async function (productName) {
+    const dashBoardPage = this.poManager.getDashboarPage();
+    await dashBoardPage.searchProductAddCart(productName);
     await dashBoardPage.navigateToCart();
-    return 'pending';
 });
 
-Then('Verify {string} is displayed in the cart', async function (string) {
-    await page.locator("div li").first().waitFor();
-    const bool = await page.locator("h3:has-text('zara coat 3')").isVisible();
+Then('Verify {string} is displayed in the cart', async function (productName) {
+    await this.page.locator("div li").first().waitFor();
+    const bool = await this.page.locator(`h3:has-text("${productName}")`).isVisible();
     expect(bool).toBeTruthy();
-    return 'pending';
 });
 
